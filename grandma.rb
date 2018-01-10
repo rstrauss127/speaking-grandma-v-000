fc5e72e68fd89e4c447b97911a30c6585ad07d78
Rebecca Strauss <rstrauss127@gmail.com>
2018-01-10 22:00:15 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,9 @@ def speak_to_grandma(phrase)
   capitalize = phrase.upcase
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN"
+  end
   if phrase == capitalize
     return "NO, NOT SINCE 1938!" 
   else return "HUH?! SPEAK UP, SONNY!"
+  end
 end
